a8a398371718df35dbf39138b7115aa49ba05a42
MD MOMENUL ISLAM BHUIYAN <nahid002345@gmail.com>
2018-03-13 15:22:46 +0100

    converting multiple xml files to single csv file like convert_xml2csv('H:\\University of Passau\\Projects\\Text Mining\\corpus\\corpus\\corpus\\citations_class_updated', 'H:\\University of Passau\\Projects\\Text Mining\\')

--- bin/__init__.py
+++ bin/__init__.py
@@ -1,17 +1,22 @@
 import numpy
 import scipy
+import numpy as np
+from xml2csv import convert_xml2csv
+
+np.set_printoptions(threshold=np.inf)
 
 from feature_extraction import count_vectorizer_feature_vector, tf_idf_trans_feature_vector, tf_idf_vect_feature_vector, \
     word2vec_feature_vector
 from clustering_function import get_cluster_kmeans
 
-vector = count_vectorizer_feature_vector()
-print('tf-idf vector')
-matrix = tf_idf_vect_feature_vector()
-no_cluster = 4
-cluster = get_cluster_kmeans(vector, no_cluster)
-print(cluster)
-cluster = get_cluster_kmeans(matrix, no_cluster)
-print(cluster)
+# vector = count_vectorizer_feature_vector()
+# print('tf-idf vector')
+# matrix = tf_idf_vect_feature_vector()
+# no_cluster = 4
+# cluster = get_cluster_kmeans(vector, no_cluster)
+# print(cluster)
+# cluster = get_cluster_kmeans(matrix, no_cluster)
+# print(cluster)
 # word2vec_feature_vector()
+
 
