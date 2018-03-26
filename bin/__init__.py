e53fbc6a3194f0b39d2446d51825b1e8581d20da
sumeet singh <singhsumeet2007@gmail.com>
2018-03-26 17:48:20 +0200

    - Linkage clustering
    added distance matrix

--- bin/__init__.py
+++ bin/__init__.py
@@ -1,25 +1,27 @@
 import numpy
 import scipy
 import numpy as np
-from xml2csv import convert_xml2csv
 
 np.set_printoptions(threshold=np.inf)
 
-from feature_extraction import count_vectorizer_feature_vector, tf_idf_trans_feature_vector, tf_idf_vect_feature_vector, \
-    word2vec_feature_vector,cluster_indices
-from clustering_function import get_cluster_kmeans,get_dbscan_cluster,tsne_reduction
+from feature_extraction import count_vectorizer_feature_vector, tf_idf_vect_feature_vector, \
+    word2vec_feature_vector
 
 from data_import import data_csv_import
+from skbio import DistanceMatrix
+
+from sklearn.metrics.pairwise import euclidean_distances
+
 
 #vector = count_vectorizer_feature_vector()
 # print('tf-idf vector')
-matrix = tf_idf_vect_feature_vector()
+#matrix = tf_idf_vect_feature_vector()
 
-#
+
 
 no_cluster = 9
-cluster = get_cluster_kmeans(matrix, no_cluster)
-print(cluster)
+#cluster = get_cluster_kmeans(matrix, no_cluster)
+#print(cluster)
 # cluster = get_cluster_kmeans(matrix, no_cluster)
 # print(cluster)
 # cluster = get_dbscan_cluster(matrix,1)
@@ -27,5 +29,17 @@ print(cluster)
 # print(cluster)
 
 # word2vec_feature_vector()
+
+txt = data_csv_import('20180313151844.csv')
+vec = tf_idf_vect_feature_vector()
+#print(vec)
+matrix =euclidean_distances(vec)
+print(matrix)
+#dmt = DistanceMatrix1(matrix)
+
+print(dmt)
+
+#hc = HClust(dmt)
+#cluster = HClust.clusters
 
 
