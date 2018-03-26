85422afc929c14893d170c817bda0b3d4872a19b
sumeet singh <singhsumeet2007@gmail.com>
2018-03-26 20:41:56 +0200

    - Linkage clustering
    added Euclidean   distance matrix

--- bin/__init__.py
+++ bin/__init__.py
@@ -1,17 +1,8 @@
-import numpy
-import scipy
 import numpy as np
 
 np.set_printoptions(threshold=np.inf)
 
-from feature_extraction import count_vectorizer_feature_vector, tf_idf_vect_feature_vector, \
-    word2vec_feature_vector
-
-from data_import import data_csv_import
-from skbio import DistanceMatrix
-
-from sklearn.metrics.pairwise import euclidean_distances
-
+from feature_extraction import dissimalrity_matrix
 
 #vector = count_vectorizer_feature_vector()
 # print('tf-idf vector')
@@ -30,14 +21,13 @@ no_cluster = 9
 
 # word2vec_feature_vector()
 
-txt = data_csv_import('20180313151844.csv')
-vec = tf_idf_vect_feature_vector()
-#print(vec)
-matrix =euclidean_distances(vec)
-print(matrix)
+#txt = data_csv_import('20180313151844.csv')
+vec = dissimalrity_matrix()
+print(vec)
+
 #dmt = DistanceMatrix1(matrix)
 
-print(dmt)
+#print(dmt)
 
 #hc = HClust(dmt)
 #cluster = HClust.clusters
