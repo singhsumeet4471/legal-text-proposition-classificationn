c44b5f128a8b688b11f7d5a4b1bf7fba471398ba
sumeet singh <singhsumeet2007@gmail.com>
2018-04-02 23:07:59 +0200

    -Evaluation table

--- bin/clustering_evaluation.py
+++ bin/clustering_evaluation.py
@@ -1,25 +1,30 @@
+import pandas as pd
 from sklearn import metrics
 
 
 def clust_eval(true, pred):
     x = metrics.adjusted_rand_score(true, pred)
     print(" Adjusted Rand index Score is : %.2f " % x)
-    rand_score = 'Adjusted Rand index Score : %.2f ' % x
+    rand_score = x
     x = metrics.adjusted_mutual_info_score(true, pred)
     print(" Mutual Information based scores is : ", x)
-    mutual_info_score = '\n Mutual Information based scores is :%.2f ' % x
+    mutual_info_score = x
     x = metrics.homogeneity_score(true, pred)
     print(" Homogeneity scores is: ", x)
-    homogeneity_score = '\n Homogeneity scores is: %.2f ' % x
+    homogeneity_score = x
     x = metrics.completeness_score(true, pred)
     print(" completeness scores is: ", x)
-    completeness_score = '\n completeness scores is: %.2f ' % x
+    completeness_score = x
     x = metrics.v_measure_score(true, pred)
     print(" V_Measure scores is: ", x)
-    v_measure_score = '\n V_Measure scores is: %.2f ' % x
+    v_measure_score =  x
     x = metrics.fowlkes_mallows_score(true, pred)
     print(" Fowlkes-Mallows scores is: ", x)
-    fowlkes_mallows_score = '\n Fowlkes-Mallows scores is: %.2f ' % x
+    fowlkes_mallows_score = x
+    d = {'Adjusted Rand index': [rand_score], 'Mutual Information based': [mutual_info_score], 'Homogeneity scores': [homogeneity_score],
+         'completeness scores':[completeness_score], 'V_Measure scores':[v_measure_score],'Fowlkes-Mallows scores':[fowlkes_mallows_score]}
+    df = pd.DataFrame(data=d)
+    print(df)
     cluste_value = rand_score + mutual_info_score + homogeneity_score + completeness_score +v_measure_score + fowlkes_mallows_score
 
     return cluste_value
